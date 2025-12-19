ab2d59445d113ff352ac570d3ecdefc724082764
chako0630 <sato37426@gmail.com>
2025-12-19 15:11:23 +0900

    Issue4 レビュー結果の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,7 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
 
-  validates :password, presence: true,
-                       format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/, message: 'は半角英数字の両方を含めて設定してください' }
+  validates :password, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/, message: 'は半角英数字の両方を含めて設定してください' }
 
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
     validates :last_name
