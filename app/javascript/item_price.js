250af0a11d11a0e6eda83f727595dbecf79a2da7
chako0630 <sato37426@gmail.com>
2026-01-31 15:37:12 +0900

    Issue5 商品出品機能の実装

---
+++ app/javascript/item_price.js
@@ -0,0 +1,24 @@
+const price = () => {
+    const priceInput = document.getElementById("item-price");
+    priceInput.addEventListener("input", () => {
+      const inputValue = priceInput.value;
+      const addTaxDom = document.getElementById("add-tax-price");
+      addTaxDom.innerHTML = calculateTax(inputValue)
+      const addProfitDom = document.getElementById("profit");
+      addProfitDom.innerHTML = calculateProfit(inputValue, calculateTax(inputValue))
+  })
+
+  function calculateTax(price) {
+    const tax = Math.floor(price * 0.1);
+    return tax;
+  }
+
+  function calculateProfit(price, tax) {
+    const profit = price - tax;
+    return profit;
+  }
+
+};
+
+window.addEventListener("turbo:load", price);
+window.addEventListener("turbo:render", price);
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,21 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :item_status
+  belongs_to :shipping_fee
+  belongs_to :prefecture
+  belongs_to :shipping_day
+
+  validates :image,            presence: true
+  validates :item_name,        presence: true
+  validates :description,      presence: true
+  validates :category_id,      numericality: { other_than: 1, message: "can't be blank" }
+  validates :item_status_id,   numericality: { other_than: 1, message: "can't be blank" }
+  validates :shipping_fee_id,  numericality: { other_than: 1, message: "can't be blank" }
+  validates :prefecture_id,    numericality: { other_than: 1, message: "can't be blank" }
+  validates :shipping_day_id,  numericality: { other_than: 1, message: "can't be blank" }
+  validates :price,            presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_than_or_equal_to: 9_999_999 }
+  
 end
