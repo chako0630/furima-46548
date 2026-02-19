e766a22548b95c0edac57bd7ce25c7bcab8b5f1f
chako0630 <sato37426@gmail.com>
2026-02-19 13:19:55 +0900

    レビュ後の修正

--- app/controllers/order_histories_controller.rb
+++ app/controllers/order_histories_controller.rb
@@ -44,7 +44,8 @@ class OrderHistoriesController < ApplicationController
   end
 
   def move_to_index
-    if current_user.id != @item.user_id || @item.order_history.present?
+    return unless current_user.id != @item.user_id || @item.order_history.present?
+
     redirect_to root_path
   end
 end
